7db8d9d8a71610b021751bf47cfcc42680c6ed23
Yomamaeatstoes <114538961+Yomamaeatstoes@users.noreply.github.com>
2023-12-03 17:10:13 -0500

    Redid 1.20.2 support
    Redo

---
+++ worldedit-bukkit/adapters/adapter-1_20_2/build.gradle.kts
@@ -0,0 +1,18 @@
+import io.papermc.paperweight.userdev.PaperweightUserDependenciesExtension
+
+plugins {
+    java
+}
+
+applyPaperweightAdapterConfiguration()
+
+repositories {
+    gradlePluginPortal()
+}
+
+dependencies {
+    // https://repo.papermc.io/service/rest/repository/browse/maven-public/io/papermc/paper/dev-bundle/
+    // // the<PaperweightUserDependenciesExtension>().foliaDevBundle("1.20.2-R0.1-SNAPSHOT")
+    the<PaperweightUserDependenciesExtension>().paperDevBundle("1.20.2-R0.1-20231125.095734-103")
+    compileOnly(libs.paperlib)
+}
